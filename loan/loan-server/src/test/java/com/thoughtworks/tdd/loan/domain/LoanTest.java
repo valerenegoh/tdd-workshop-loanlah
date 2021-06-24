39f930e867b9c45f89e00fd9104c507ab5983640
Priti Biyani <priti.biyani6@gmail.com>
2021-06-24 16:58:30 +0800

    Vinh/Priti/Don: Use LoanBuilder instead of using Loan directly.

--- loan/loan-server/src/test/java/com/thoughtworks/tdd/loan/domain/LoanTest.java
+++ loan/loan-server/src/test/java/com/thoughtworks/tdd/loan/domain/LoanTest.java
@@ -1,56 +1,54 @@
 package com.thoughtworks.tdd.loan.domain;
 
 import com.thoughtworks.tdd.loan.utils.LoanBuilder;
-import com.thoughtworks.tdd.loan.utils.Stubs;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class LoanTest {
 
-  @Test
-  void shouldNotBeAbleToCreateLoanWithNegativeAmount() {
-    assertThrows(IllegalArgumentException.class, () -> new LoanBuilder().withAmount(-1).build());
-  }
+    @Test
+    void shouldNotBeAbleToCreateLoanWithNegativeAmount() {
+        assertThrows(IllegalArgumentException.class, () -> new LoanBuilder().withAmount(-1).build());
+    }
 
-  @Test
-  void shouldNotBeAbleToCreateLoanWithNegativeDuration() {
-    assertThrows(IllegalArgumentException.class, () -> new LoanBuilder().withDurationInDays(-1).build());
-  }
+    @Test
+    void shouldNotBeAbleToCreateLoanWithNegativeDuration() {
+        assertThrows(IllegalArgumentException.class, () -> new LoanBuilder().withDurationInDays(-1).build());
+    }
 
-  @Test
-  void shouldNotBeAbleToCreateLoanWithoutAccountNumber() {
-    assertThrows(NullPointerException.class, () -> new LoanBuilder().withAccount(null).build());
-  }
+    @Test
+    void shouldNotBeAbleToCreateLoanWithoutAccountNumber() {
+        assertThrows(NullPointerException.class, () -> new LoanBuilder().withAccount(null).build());
+    }
 
-  @Test
-  void shouldNotBeAbleToCreateLoanWithoutTakenAt() {
-    assertThrows(NullPointerException.class, () ->new LoanBuilder().withTakenOn(null).build());
-  }
+    @Test
+    void shouldNotBeAbleToCreateLoanWithoutTakenAt() {
+        assertThrows(NullPointerException.class, () -> new LoanBuilder().withTakenOn(null).build());
+    }
 
-  @Test
-  void shouldCalculateOutstandingAmountForOneYear() {
-    Loan loan = new Loan(Stubs.id(), Stubs.uuid(), 1000, LocalDate.now(), 365);
+    @Test
+    void shouldCalculateOutstandingAmountForOneYear() {
+        Loan loan = new LoanBuilder().withAmount(1000).withDurationInDays(365).build();
 
-    assertThat(loan.totalOutstanding()).isEqualByComparingTo(new BigDecimal("1100.00"));
-  }
+        assertThat(loan.totalOutstanding()).isEqualByComparingTo(new BigDecimal("1100.00"));
+    }
 
-  @Test
-  void shouldCalculateOutstandingAmountFor10Days() {
-    Loan loan = new Loan(Stubs.id(), Stubs.uuid(), 1000, LocalDate.now(), 10);
+    @Test
+    void shouldCalculateOutstandingAmountFor10Days() {
+        Loan loan = new LoanBuilder().withAmount(1000).withDurationInDays(10).build();
 
-    assertThat(loan.totalOutstanding()).isEqualByComparingTo(new BigDecimal("1005.48"));
-  }
+        assertThat(loan.totalOutstanding()).isEqualByComparingTo(new BigDecimal("1005.48"));
+    }
 
-  @Test
-  void shouldCalculateOutstandingAmountFor100Days() {
-    Loan loan = new Loan(Stubs.id(), Stubs.uuid(), 1000, LocalDate.now(), 100);
+    @Test
+    void shouldCalculateOutstandingAmountFor100Days() {
+        Loan loan = new LoanBuilder().withAmount(1000).withDurationInDays(100).build();
 
-    assertThat(loan.totalOutstanding()).isEqualByComparingTo(new BigDecimal("1041.10"));
-  }
+        assertThat(loan.totalOutstanding()).isEqualByComparingTo(new BigDecimal("1041.10"));
+    }
 
 }
