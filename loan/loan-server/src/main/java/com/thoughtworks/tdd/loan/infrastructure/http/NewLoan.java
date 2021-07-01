7fc2728b0a651f8f015f20e24961f629abd75b31
Donatien <donatien.couka@thoughtworks.com>
2021-07-01 18:14:36 +0800

    Add Constructor to NewLoan

--- loan/loan-server/src/main/java/com/thoughtworks/tdd/loan/infrastructure/http/NewLoan.java
+++ loan/loan-server/src/main/java/com/thoughtworks/tdd/loan/infrastructure/http/NewLoan.java
@@ -1,8 +1,15 @@
 package com.thoughtworks.tdd.loan.infrastructure.http;
 
+import java.util.Objects;
+
 public class NewLoan {
   private int amount;
   private int durationInDays;
+
+  public NewLoan(int amount, int durationInDays) {
+    this.amount = amount;
+    this.durationInDays = durationInDays;
+  }
 
   public int getAmount() {
     return amount;
@@ -18,5 +25,18 @@ public class NewLoan {
 
   public void setDurationInDays(int durationInDays) {
     this.durationInDays = durationInDays;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    NewLoan newLoan = (NewLoan) o;
+    return amount == newLoan.amount && durationInDays == newLoan.durationInDays;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(amount, durationInDays);
   }
 }
